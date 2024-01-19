512b724e8dc6306bd2e026f18722dfd63525261d
Justin_Birdsall <JustinLeRoyBirdsall@gmail.com>
2024-01-19 13:06:34 -0500

    webhook?

---
+++ hw1_birdsall/3.problem/main.py
@@ -0,0 +1,14 @@
+
+if __name__ == "__main__":
+    num_contestants = int(input())
+
+    for test_idx in range(num_tests): 
+        a = 0
+#@TODO 
+# my thinking for this problem is this         
+# figure out how to take the data that is imported in from the users and that its seperated by spaces
+# as the data is being read add the # to a dictionary as key
+# if the number appears again then increase the dictionary value by 1
+# looking at the constraints get loop through and see if there contains a key based on incramenter 
+# if it does then print and pop value `` 
+print(a)
